075e3e57e330d71b398c2c1423ba029ecd0d09a9
arielfc <48219012+arielfc@users.noreply.github.com>
2019-03-15 14:17:53 -0600

    updated day 9 assignment

---
+++ TravelExpertsPHP/TravelExperts/header.php
@@ -0,0 +1,14 @@
+
+<header>
+    <img src="assets\images\logowhite.png">
+    <nav id="container" class="center">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="links.php">Links</a></li>
+            <li><a href="destinations.php">Destinations</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="register.php">Register</a></li>
+            <li><a href="about.php">About</a></li>
+        </ul>
+    </nav>
+</header>
